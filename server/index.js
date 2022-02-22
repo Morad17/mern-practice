28086e9af2999f91dec5f78934f243e604a868a7
Morad17 <morad123@hotmail.co.uk>
2022-02-22 18:51:58 +0000

    initial commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/hello', (req,res) => {
+    res.send('hello world')
+})
+
+app.listen(1337, () => {
+    console.log('Server has started on 1337')
+})
