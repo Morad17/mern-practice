369f44db0c2a5046e9639f1f8ed8b65ce2687825
Morad17 <morad123@hotmail.co.uk>
2022-02-24 12:22:55 +0000

    updates

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
-
+const dotenv = require("dotenv");
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+dotenv.config()
+
+mongoose.connect( process.env.CONNECTION_URL 
+,{  useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
 app.use(express.json())
+
+
 
 app.post('/api/register', (req,res)=>{
     console.log(req.body)
